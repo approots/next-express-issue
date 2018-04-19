f4c2ebec650bf906595b7973c2690d68519a8a8c
rick <rick@coalescentdesign.com>
2018-04-18 21:59:10 -0700

    Initial Commit.

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+export default class extends React.Component {
+  static getInitialProps({ query: { id } }) {
+    console.log("getInitialProps index");
+    return {};
+  }
+
+  render() {
+    return (
+      <ul>
+        <li>
+          <Link href="/b" as="/a">
+            <a>a</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/a" as="/b">
+            <a>b</a>
+          </Link>
+        </li>
+        <li>
+          <Link href={{ pathname: "/posts", query: { id: "2" } }} as="/posts/2">
+            <a>post #2</a>
+          </Link>
+        </li>
+      </ul>
+    );
+  }
+}
